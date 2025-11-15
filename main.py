b55d5f4618904f199b483d963744a0763ba7aee5
xaviax <liquidcloud0@gmail.com>
2025-11-15 21:36:20 +0500

    initial stage 1

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill('black')
-    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill('black')
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
