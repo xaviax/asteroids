e92d8edf7b9b1bf18e05c8c78dc58d17a14524b7
xaviax <liquidcloud0@gmail.com>
2025-11-15 19:41:35 +0500

    initial setup

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+
+def main():
+    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill('black')
+    pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
